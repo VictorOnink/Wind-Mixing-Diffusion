5ac296a005e41527cf40e7f19987f809319c8688
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-11-19 10:54:00 +0100

    starting to create teh code

---
+++ parcels_simulation.py
@@ -0,0 +1,13 @@
+"""
+We are going to be doing highly idealised Parcels simulations, with the end goal being to develop a parametrization
+for wind mixing for use with particle simulations.
+
+"""
+
+from parcels import FieldSet, ParticleSet, Variable, JITParticle
+import numpy as np
+
+def vertical_diffusion_run(k_h):
+
+
+def simple_vertical_diffusion
